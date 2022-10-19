88176522f6f82d232c5cf36465c665edb20d9a59
Francisco De Los Santos <frandelosantoss12@gmail.com>
2022-10-19 20:09:39 -0300

    Elimina salida de la fuente del programa en TS

--- dist/config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var config;
-(function (config) {
-    config["host"] = "localhost";
-    config[config["controllerPort"] = 3000] = "controllerPort";
-    config[config["clientPort"] = 5000] = "clientPort";
-})(config || (config = {}));
-exports.default = config;
--- dist/src/components/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.App = void 0;
-var app_component_1 = require("./app.component");
-Object.defineProperty(exports, "App", { enumerable: true, get: function () { return __importDefault(app_component_1).default; } });
